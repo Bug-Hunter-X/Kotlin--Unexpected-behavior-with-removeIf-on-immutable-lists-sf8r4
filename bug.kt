70e4c6246f9b31a9cb2419900c73b290f799edb0
BugHunter-X <askzenai@gmail.com>
2025-01-02 22:24:05 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,10 @@
+```kotlin
+fun main() {
+    val list = mutableListOf(1, 2, 3, 4, 5)
+    list.removeIf { it > 2 }
+    println(list) // Output: [1, 2]
+
+    val list2 = listOf(1, 2, 3, 4, 5)
+    // list2.removeIf { it > 2 } // This will cause a compile-time error because listOf is immutable
+}
+```
